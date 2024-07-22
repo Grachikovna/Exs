e75c6b74855b39f9321895aeb59879d9a4f3e957
origin <maryna.oganesyan@yandex.ru>
2024-07-22 21:33:21 +0400

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+    private static final String REGEX = "^[a-zA-Z0-9_]*$";
+    private static final String AllLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            confirmationOfDate("Rima", "qwerty_22", "qwerty_22");
+            System.out.println("Проверка пройдена");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Проверка не пройдена");
+            e.printStackTrace();
+        }
     }
+
+    public static void confirmationOfDate(String login, String password, String confirmPassword) {
+        if (login.length() > 20) {
+            throw new WrongLoginException("Длина логина больше 20");
+        }
+        if (!login.matches(REGEX)) {
+            throw new WrongLoginException("В логине недопустмые символы");
+        }
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Длина пароля больше 20");
+        }
+        if (!password.matches(REGEX)) {
+            throw new WrongPasswordException(("В пароле недопустмые символы"));
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли друг с другом не совпадают");
+        }
+    }
+
 }
+
